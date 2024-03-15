cef8769458afae5f615a990476cd6d520c354ecc
aalvaropc <aalvaropc@gmail.com>
2024-03-15 12:30:01 -0500

    edit questions and answers

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import re
 import streamlit as st
 from utils import calculate_security_score, show_diagnosis
 
@@ -44,6 +45,10 @@ def main():
             "question": "¿Cuál de las siguientes prácticas de seguridad cibernética realiza regularmente?",
             "type": "multiselect",
             "options": ["Realizar copias de seguridad", "Utilizar VPN", "Actualizar software regularmente", "Utilizar autenticación de dos factores", "Revisar permisos de aplicaciones", "Utilizar administrador de contraseñas"]
+        },
+        {
+            "question": "Por favor, ingrese el correo electrónico:",
+            "type": "text"
         }
     ]
 
@@ -62,9 +67,30 @@ def main():
         elif question_type == "selectbox":
             answer = st.selectbox(question, options=question_data["options"])
             answers[question] = answer
+        elif question_type == "text":
+            answer = st.text_input(question)
+            answers[question] = answer
 
     if st.button("Enviar"):
-        show_diagnosis(answers)
+        if validate_institutional_email(answers.get("Por favor, ingrese el correo electrónico:", "")):
+            show_diagnosis(answers)
+            st.success("Correo institucional verificado.")
+        else:
+            show_diagnosis(answers)
+            st.error("¡ALERTA DE FISHING! Este correo no pertenece a la empresa.")
+
+def validate_institutional_email(email):
+    # Expresión regular para verificar el formato del correo electrónico
+    email_regex = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+    
+    # Verifica si el correo electrónico sigue el formato correcto
+    if re.match(email_regex, email):
+        # Verifica si el dominio del correo electrónico es institucional
+        institutional_domains = ["unica.edu.pe", "sistemasunica.edu.pe"]
+        domain = email.split('@')[-1]
+        if domain in institutional_domains:
+            return True
+    return False
 
 if __name__ == "__main__":
     main()
